151189d762e2d4db52d83b806c21b4bc67bdf92e
Srisivan <srisivan.k@gmail.com>
2017-09-24 08:18:09 +0530

    Variable, Types, Expressions, and statements

--- Exercise1.py
+++ Exercise1.py
@@ -29,3 +29,32 @@ type(1234)                    	# Outputs 'int'
 type(3.1456)                  	# Outputs 'float'
 type('123456')                  # DOES NOT Output as 'int'. Outputs as 'str', as they are inside quotes
 
+
+# 2. Variables
+
+'''
+Variables are the most important of all programming languages. It is simply like a container, that holds values inside. If it is assigned a new value, 
+the old one is discarded, and replaced by the new one.
+
+'''
+
+# We can declare a new variable like this :
+
+interesting_word = 'Scrumdidllyumptious'         # Assingns the value 'Scrumdidllyumptious' to the variable interesting_word.
+print(interesting_word)                          # Prints 'Scrumdidllyumptious'
+
+interesting_word = 'Babblement'                  # Assingns the value 'Babblemnt' to the variable interesting_word. The old one is discarded.
+print(interesting_word)                          # DOES NOT Print 'Scrumdidllyumptious'. It prints 'Babblement'.
+
+# Variables can also be assigned integers as values :
+
+interesting_number = 20
+
+# They can also be used for operations
+
+ print(interesting_number + 23)                 # Prints 43. (20 + 23)
+
+ # .... And can also be assigned to Decimeals :
+
+ pi = 3.14124787
+
