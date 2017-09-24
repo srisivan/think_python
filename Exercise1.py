5ef3d903238ca4ab062a050edc60b0fb269988d9
Srisivan <srisivan.k@gmail.com>
2017-09-24 08:01:53 +0530

    Variable, Types, Expressions, and statements

---
+++ Exercise1.py
@@ -0,0 +1,31 @@
+
+# Working out Exercises and problems from the book Think Python
+
+# Chapter 1
+# Variables, Expressions, and Statements
+
+# 1. Values and Types
+
+''' 
+The Values in python are :
+
+1. String
+2. Integers
+3. Float
+
+
+The Types in python are :
+
+1. str for String
+2. int for Integers
+3. float for Decimeals
+
+'''
+ 
+# We can find which type a object is, by typing it in the type() command, like this::
+
+type('Hello World!')            # Outputs 'str'
+type(1234)                    	# Outputs 'int'
+type(3.1456)                  	# Outputs 'float'
+type('123456')                  # DOES NOT Output as 'int'. Outputs as 'str', as they are inside quotes
+
